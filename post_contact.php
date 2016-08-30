86da043c9f81e40691df0db7e94215880806a23f
rasho22 <ab_rasho@yahoo.fr>
2016-08-30 14:43:32 +0200

    finalisation

--- post_contact.php
+++ post_contact.php
@@ -7,15 +7,15 @@
 		$errors['name']="Vous n'avez pas mis votre nom!";
 	}
 
-	if (!array_key_exists('email', $_POST) || $_POST['email'] =='' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	if (!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 		$errors['email']="Vous n'avez pas mis votre email!";
 	}
-	if (!array_key_exists('message', $_POST) || $_POST['message'] =='') {
+	if (!array_key_exists('message', $_POST) || $_POST['message'] == '') {
 		$errors['message']="Vous n'avez pas mis un message!";
 	}
-	if (!empty($errors)) {
 
-		session_start();
+	session_start();
+	if (!empty($errors)) {
 
 		$_SESSION['errors']=$errors;
 		$_SESSION['inputs']=$_POST;
@@ -25,48 +25,10 @@
 	{
 		$_SESSION['success']=1;
 		$message=$_POST['message'];
-		$headers='FROM:ab_rasho@yahoo.fr';
-		mail('ab_rasho@yahoo.fr', 'formaulaire contact', $message,$headers);
+		$headers='FROM: ab_rasho@yahoo.fr';
+		mail($_POST['email'], 'formaulaire de contact', $message,$headers);
 		header('Location:mail.php');
 
 	}
-	/*
-
-<?php
-     $to      = 'ab_rasho@yahoo.fr';
-     $name = $_POST['name'];
-     $message = $_POST[message];
-     $headers = 'From: 'ab_rasho@yahoo.fr' . "\r\n" .
-     'Reply-To: webmaster@example.com' . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
-
-     mail($to, $subject, $message, $headers);
- ?>
-
-
-	$nom=$HTTP_POST_VARS['name']; 
-$mail=$HTTP_POST_VARS['email'];  
-$message=$HTTP_POST_VARS['message']; 
-
-/////voici la version Mine 
-$headers = "MIME-Version: 1.0\r\n"; 
-
-//////ici on détermine le mail en format text 
-$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n"; 
-
-////ici on détermine l'expediteur et l'adresse de réponse 
-$headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP"; 
-
-$subject="$objet"; 
-$destinataire="ab_rasho@yahoo.fr"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
-$body="$message"; 
-if (mail($destinataire,$subject,$body,$headers)) { 
-echo "Votre mail a été envoyé<br>"; 
-} else { 
-echo "Une erreur s'est produite"; 
-} 
-
-/*
-*/
-	
+		
 ?>
